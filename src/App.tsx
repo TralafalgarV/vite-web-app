6adf6a5991c64f522a68d7bb44a74573d9ec19f2
王威1 <wangwei1@hualala.com>
2021-06-30 18:18:09 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import style from "./App.module.scss";
+
+function App() {
+  return (
+    <div className={style.App}>
+      <header className={style.AppHeader}>
+        <div>Typescript + Vite2</div>
+      </header>
+    </div>
+  )
+}
+
+export default App
